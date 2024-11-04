e8d92481a3101f994b552c21f1c1e9125a4360a8
Your <2535985035@qq.com>
2024-11-04 15:19:03 +0800

    aa4

--- assets/effect.js
+++ assets/effect.js
@@ -176,8 +176,8 @@ $('document').ready(function(){
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+			if(i==12){
+				$("p:nth-child(11)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
 				});
 				
